f06ff0fb4d364dafb09b17b45973527b3bebfe34
tsejunzhen <tsejunzhen95@hotmail.com>
2020-08-11 15:34:32 +0800

    First commit

---
+++ src/test/java/com/victor/reactspring/ReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.reactspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
